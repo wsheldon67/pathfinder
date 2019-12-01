2235a24b4c44ba159cf5713ead176ef57ad3fb0c
William <ws.livelaughlove@gmail.com>
2019-11-30 22:48:43 -0600

    character create submenu
    Added Create as submenu of Character Manager

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import skills
-import characterManager
+import createCharacter
 
 # tk window
 root = Tk()
@@ -11,14 +11,23 @@ mainframe.grid()
 # tk notebook
 n = ttk.Notebook(mainframe)
 n.grid(row=0, column=0)
+
+# skills
 skillFrame = ttk.Frame(n)
 skillFrame.grid()
+n.add(skillFrame, text='Skills')
+skills.post(skillFrame)
+
+# character manager
 characterManagerFrame = ttk.Frame(n)
 characterManagerFrame.grid()
-n.add(skillFrame, text='Skills')
 n.add(characterManagerFrame, text='Manage Characters')
-
-skills.post(skillFrame)
-characterManager.post(characterManagerFrame)
+characterManagerNotebook = ttk.Notebook(characterManagerFrame)
+characterManagerNotebook.grid(row=0, column=0)
+# character create
+createCharacterFrame = ttk.Frame(characterManagerNotebook)
+createCharacterFrame.grid()
+characterManagerNotebook.add(createCharacterFrame, text='Create')
+createCharacter.post(createCharacterFrame)
 
 root.mainloop()
